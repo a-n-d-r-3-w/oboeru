72dbdbcd3487eb2ae65c9c33e1fe37988e0a2530
Andrew <andrew.liu01@libertymutual.com>
2018-09-13 12:23:17 -0400

    Insert documents

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const insertDocuments = async db => {
+  const collection = db.collection('collection');
+  let result;
+  try {
+    result = await collection.insertMany([
+      { a: 1 },
+      { a: 2 },
+      { a: 3 },
+    ]);
+    assert.equal(3, result.result.n)
+    assert.equal(3, result.ops.length)
+    console.log('Inserted 3 documents into the collection')
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
 (async () => {
   const url = 'mongodb://localhost:27017';
   const dbName = 'oboeru';
@@ -10,6 +27,7 @@ const assert = require('assert');
     client = await MongoClient.connect(url, { useNewUrlParser: true });
     console.log('Opened connection to mongodb');
     const db = client.db(dbName);
+    insertDocuments(db);
   } catch (err) {
     console.log(err.stack);
   }

f036e0fd59d87272d06280c476dccf3702c6ebe7
Andrew <andrew.liu01@libertymutual.com>
2018-09-13 12:42:49 -0400

    Add users and find documents

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const COLLECTION_NAME = 'users';
+const DB_NAME = 'oboeru';
 
 const insertDocuments = async db => {
-  const collection = db.collection('collection');
-  let result;
-  try {
-    result = await collection.insertMany([
-      { a: 1 },
-      { a: 2 },
-      { a: 3 },
-    ]);
-    assert.equal(3, result.result.n)
-    assert.equal(3, result.ops.length)
-    console.log('Inserted 3 documents into the collection')
-  } catch (err) {
-    console.log(err.stack);
-  }
-}
+  const collection = db.collection(COLLECTION_NAME);
+  const result = await collection.insertMany([
+    { username: 'wolfwire', passwordHash: 'w01fw1r3' },
+    { username: 'brainstorm', passwordHash: 'br@in$+0rm' },
+  ]);
+  assert.equal(3, result.result.n)
+  assert.equal(3, result.ops.length)
+  console.log('Inserted 3 documents into the collection')
+};
+
+const findDocuments = async db => {
+  const collection = db.collection(COLLECTION_NAME);
+  const documents = await collection.find({}).toArray();
+  console.log(documents)
+};
 
 (async () => {
   const url = 'mongodb://localhost:27017';
-  const dbName = 'oboeru';
   let client;
 
   try {
     client = await MongoClient.connect(url, { useNewUrlParser: true });
     console.log('Opened connection to mongodb');
-    const db = client.db(dbName);
+    const db = client.db(DB_NAME);
     insertDocuments(db);
+    findDocuments(db);
   } catch (err) {
     console.log(err.stack);
   }

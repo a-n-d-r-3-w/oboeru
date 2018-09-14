ba73227bbd54e9b19a10dc0c23e445c454bf26fe
andr3w <liu.anray+github@gmail.com>
2018-09-13 21:53:31 -0400

    Install Express and write an API test using supertest

---
+++ api/index.spec.js
@@ -0,0 +1,21 @@
+/* globals afterEach, beforeEach, expect, test */
+/* eslint-disable global-require */
+// This test requires the Express server to be running.
+const request = require('supertest');
+
+// request = request('http://localhost:3000');
+
+let server;
+
+beforeEach(async () => {
+  server = require('./index');
+});
+
+afterEach(async () => {
+  server.close();
+});
+
+test('smoke', async () => {
+  const response = await request(server).get('/');
+  expect(response.statusCode).toBe(200);
+});
